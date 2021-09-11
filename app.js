e670e507ba595271ceef4c5272cbfd0e2b0821e2
Manan <mananman23@gmail.com>
2021-09-11 22:32:08 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -22,3 +22,46 @@ app.get('/', function(req, res){
 app.get('/tour/:name', function(req,res){
     res.render('tour', {info: tour[req.params.name], name: req.params.name})
 })
+
+
+app.get('/shop', function (req, res) {
+    res.render('shop')
+})
+
+
+app.get('/shop/checkcout', function (req, res) {
+    res.render('checkout')
+})
+
+
+// Twilio
+const accountSid = 'AC79a1a3c005662307dab23608e30aaa66';
+const authToken = '6e72e06be10c5a4764d012a8f8bf1cad';
+const client = require('twilio')(accountSid, authToken);
+
+
+app.post('/book', function(req, res){
+    client.messages
+        // from: '+16692894956',
+        .create({
+            body: 'Hey' + req.body.name + ", We have recieved your booking for going to " + req.body.planet + ". We'll get in touch with you soon.\nBooking ID: 202109007.\nThanks,\nGalactic Tour",
+            messagingServiceSid: 'MG3035fbd0df5faea01bd3e78d97dd801c',
+            to: req.body.phonenumber
+        })
+        .then(message => console.log(message.sid))
+    .done();
+})
+
+
+// DB
+const firebase = require("firebase/app");
+const fs = require('firebase/firestore');
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase/firestore-key.json")
+
+admin.initializeApp();
+const db = admin.firestore();
+
+app.post('/shop/checkout', function (req, res) {
+
+})
